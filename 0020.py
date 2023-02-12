9830a3c446f3bd826eb41bfcdf3e84b9a29a7388
bryce <76966016+brycegallo@users.noreply.github.com>
2023-02-11 23:49:36 -0500

    Update 0020 - Total: 31 added missing solution from leetcode

--- 0020.py
+++ 0020.py
@@ -7,3 +7,17 @@
 #     Open brackets must be closed in the correct order.
 #     Every close bracket has a corresponding open bracket of the same type.
 #
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        hashMap = {')':"(",']':'[','}':'{'}
+
+        for i in s:
+            if i in hashMap:
+                if stack and stack[-1] == hashMap[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if not stack else False
