8ea0066dedd208ee5b1f369453b0e2da35fc8cb5
bryce <76966016+brycegallo@users.noreply.github.com>
2023-02-23 18:16:27 -0500

    complete one more js - Total: 45 complete 0206 in javascript, update python file name

---
+++ 0206js.js
@@ -0,0 +1,24 @@
+// leetcode 0206 - Reverse Linked List
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let current = head;
+    let previous = null;
+    while (current !== null) {
+        let next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+    }
+    return previous;
+};
