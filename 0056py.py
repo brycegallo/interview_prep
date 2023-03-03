88445ef3ce6261eb003d1d603d88b731ebf3e636
bryce <76966016+brycegallo@users.noreply.github.com>
2023-03-03 02:06:20 -0500

    complete one more python - Total: 80|6 complete 0056 in python

--- 0056py.py
+++ 0056py.py
@@ -1,6 +1,7 @@
 # leetcode 0057 - Insert Interval
 # Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
 # and return an array of the non-overlapping intervals that cover all the intervals in the input.
+# Time O(nlogn)
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda i: i[0])
