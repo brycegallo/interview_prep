e6f0b45aa27259d56009c3dcad6bcb7c0fee1ad6
bryce <76966016+brycegallo@users.noreply.github.com>
2023-03-03 01:44:42 -0500

    complete one more python - Total: 79|6 complete 0057 in python

---
+++ 0057py.py
@@ -0,0 +1,25 @@
+# leetcode 0057 - Insert Interval
+# You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi]
+# represent the start and the end of the ith interval and intervals is sorted in ascending order by starti.
+# You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+#
+# Insert newInterval into intervals such that intervals is still sorted in ascending order by starti
+# and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+#
+# Return intervals after the insertion.
+# Time: O(n), Memory: O(n)
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                result.append(newInterval)
+                return result + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                result.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+
+        result.append(newInterval)
+        return result
