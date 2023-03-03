491f8fcacc04809ece46405dac193b6244224100
bryce <76966016+brycegallo@users.noreply.github.com>
2023-03-02 19:35:11 -0500

    complete one more python - Total: 73|6 complete 0152 in python

---
+++ 0152py.py
@@ -0,0 +1,15 @@
+# leetcode 0152 - Maximum Product Subarray
+# Given an integer array nums, find a subarray that has the largest product, and return the product.
+# The test cases are generated so that the answer will fit in a 32-bit integer.
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        result = nums[0]
+        currentMin = currentMax = 1
+
+        for n in nums:
+            tempNcMax = currentMax * n
+            currentMax = max(currentMax * n, currentMin * n, n)
+            currentMin = min(tempNcMax, currentMin * n, n)
+            result = max(result, currentMax)
+
+        return result
