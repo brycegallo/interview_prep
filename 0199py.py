1161fff461a3aa452d224eefa1d89e1e0d8b673a
bryce <76966016+brycegallo@users.noreply.github.com>
2023-04-27 10:59:02 -0400

    complete one more python - Total: 101|10 complete 0199 in python

---
+++ 0199py.py
@@ -0,0 +1,31 @@
+# leetcode 0199 - Binary Tree Right Side View
+# Medium
+#
+# Given the root of a binary tree, imagine yourself standing on the right side of it,
+# return the values of the nodes you can see ordered from top to bottom.
+# Time: ? Memory: ?
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        q = collections.deque([root])
+
+        while q:
+            rightSide = None
+
+            for i in range(len(q)):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+
+            if rightSide:
+                result.append(rightSide.val)
+
+        return result
