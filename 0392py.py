492c07292cd1ec67ef499ae0166db63cefebbe47
bryce <76966016+brycegallo@users.noreply.github.com>
2023-05-07 16:29:56 -0400

    complete 1 more python - Total: 108|11 complete 0392 in python

---
+++ 0392py.py
@@ -0,0 +1,18 @@
+# leetcode 0392 - Is Subsequence
+# Easy
+#
+# Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+#
+# A subsequence of a string is a new string that is formed from the original string by deleting some (can be none)
+# of the characters without disturbing the relative positions of the remaining characters.
+# (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+# Time: O(n) / O(t) Memory: O(1)
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+        return True if i == len(s) else False
