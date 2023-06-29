85f8dce73b8a40395d31f9f124d41b61bfeae1f3
bryce <76966016+brycegallo@users.noreply.github.com>
2023-06-29 09:42:07 -0400

    complete 1 more python - Total: 137|11 complete 0309 in python

--- 0309py.py
+++ 0309py.py
@@ -8,4 +8,25 @@
 #     After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
 #
 # Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
-# Time: O(?) Memory: O(?)
+# Time: O(n) Memory: O(n)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # State: Buying or Selling?
+        # If Buy -> i + 1
+        # If Sell -> i + 2
+
+        dp = {}  # key=(i, looking_to_buy) val=max_profit
+
+        def dfs(i, looking_to_buy):
+            if i >= len(prices):
+                return 0
+            if (i, looking_to_buy) not in dp:
+                cooldown = dfs(i + 1, looking_to_buy)
+                if looking_to_buy:
+                    transaction = dfs(i + 1, not looking_to_buy) - prices[i]  # buying
+                else:
+                    transaction = dfs(i + 2, not looking_to_buy) + prices[i]  # selling
+                dp[(i, looking_to_buy)] = max(transaction, cooldown)
+            return dp[(i, looking_to_buy)]
+
+        return dfs(0, True)
