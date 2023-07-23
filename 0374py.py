3d8cf037721d99055c4d6597c563d3348b8906cf
bryce <76966016+brycegallo@users.noreply.github.com>
2023-07-23 16:31:38 -0400

    complete 1 more python - Total: 146|11 complete 0374 in python

---
+++ 0374py.py
@@ -0,0 +1,37 @@
+# leetcode 0374 - Guess Number Higher or Lower
+# Easy - Binary Search
+#
+# We are playing the Guess Game. The game is as follows:
+#
+# I pick a number from 1 to n. You have to guess which number I picked.
+#
+# Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+#
+# You call a pre-defined API int guess(int num), which returns three possible results:
+#
+#     -1: Your guess is higher than the number I picked (i.e. num > pick).
+#     1: Your guess is lower than the number I picked (i.e. num < pick).
+#     0: your guess is equal to the number I picked (i.e. num == pick).
+#
+# Return the number that I picked.
+# Time: O(logn) Memory: O(1)
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        low, high = 1, n
+
+        while True:
+            middle = (high + low) // 2
+            result = guess(middle)
+            if not result:
+                return middle
+            elif result < 0:
+                high = middle - 1
+            else:
+                low = middle + 1
