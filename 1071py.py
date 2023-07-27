a115b3252c41dd042ea5328395aced75dfbf6370
bryce <76966016+brycegallo@users.noreply.github.com>
2023-07-26 23:41:59 -0400

    complete 1 more python - Total: 156|11 complete 1071 in python

---
+++ 1071py.py
@@ -0,0 +1,22 @@
+# leetcode 1071 - Greatest Common Divisor of Strings
+# Easy - Math and Geometry
+#
+# For two strings s and t, we say "t divides s" if and only if s = t + ... + t
+# (i.e., t is concatenated with itself one or more times).
+#
+# Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+# Time: O( min(m,n)*(m+n) ) Memory: O()
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        len1, len2 = len(str1), len(str2)
+
+        def isDivisor(length):
+            if len1 % length or len2 % length:
+                return False
+            f1, f2 = len1 // length, len2 // length
+            return str1[:length] * f1 == str1 and str1[:length] * f2 == str2
+
+        for leng in range(min(len1, len2), 0, -1):
+            if isDivisor(leng):
+                return str1[:leng]
+        return ""
