983965832401c7fe822934d839db14a8095fe630
bryce <76966016+brycegallo@users.noreply.github.com>
2023-08-09 23:38:33 -0400

    complete 1 more js - Total: 163 = 159|15(4)|3 complete 2635 in js

---
+++ 2635js.js
@@ -0,0 +1,22 @@
+// leetcode 2635 - Apply Transform Over Each Element in Array
+ // Easy - Javascript 30-Day Challenge: Day 3
+//
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+//
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+//
+// Please solve it without the built-in Array.map method.
+// Time: O(n) Memory: O(n)
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const result = []
+
+    for (const i in arr) {
+        result.push(fn(arr[i], Number(i)));
+    }
+    return result;
+};
