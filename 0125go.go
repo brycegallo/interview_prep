04ebbc22039cdedf6a8af43d4a7febcca232aa0f
brycegallo <76966016+brycegallo@users.noreply.github.com>
2025-04-29 08:48:18 -0600

    Fix C solution mislabeled as Go solution also update some files

--- 0125go.go
+++ 0125go.go
@@ -1,3 +1,10 @@
+// Leetcode 0125 - Valid Palindromes
+// Easy - Two Pointers
+// A phrase is a palindrome if, after convering all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward.
+// Alphanumeric characters include letters and numbers
+//
+// Given a string s, return true if it is a palindrome, or false otherwise
+// Time Complexity O(n) Memory O(1)
 func isAlphaNumeric(c rune) bool {
     return unicode.IsLetter(c) || unicode.IsDigit(c)
 }
--- 0242go.go
+++ 0242go.go
@@ -1,3 +1,8 @@
+// Leetcode 0242 - Valid Anagram
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise
+// s is guaranteed to be at least 1 character, t is not
+// Solution with quicksort
+// Time Complexity: O(n logn + m logm) Space: O(1) or O(n + m) depending on algorithm
 func isAnagram(s string, t string) bool {
     if len(s) != len(t) {
 	return false
@@ -20,3 +25,5 @@ func isAnagram(s string, t string) bool {
     }
     return true
 }
+
+// a hash map solution also exists to be implemented in Go
