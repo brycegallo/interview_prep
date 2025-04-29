f1b539f155d9e64a60544287e682d43c339682a1
brycegallo <76966016+brycegallo@users.noreply.github.com>
2025-04-28 18:43:39 -0600

    Solve 0020 - Valid Parentheses in C also update comments for 0001go.go

--- 0001go.go
+++ 0001go.go
@@ -1,3 +1,10 @@
+// Leetcode 0001 - Two Sum
+// Easy - Arrays & Hashing
+// Given an array of integers num and an integer taraget, return the indices of the two numbers such that they add up to target
+// You may assume that each input would have exactly one solution, and you may not use the same element twice
+// You can return teh answer in any order
+// Time Complexity: O(n) Memory O(n)
+
 func twoSum(nums []int, target int) []int {
     // hashmap will work like indices[value] = index
     indices := make(map[int]int)
