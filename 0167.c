f4f98665d4d1c19eddb3c0415e72cebb1d322229
bryce <76966016+brycegallo@users.noreply.github.com>
2025-05-07 12:35:53 -0600

    Solve 0167 Two Sum II - Input Array is Sorted in C

--- 0167.c
+++ 0167.c
@@ -1,5 +1,5 @@
 // Leetcode 0167 - Two Sum II - Input Array is Sorted
-// Medium(?) - Two Pointers
+// Medium (not really) - Two Pointers 
 // Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
 //
 // Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
